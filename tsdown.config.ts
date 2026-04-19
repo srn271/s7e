4fd620b901a6308c94a23015a26263364c832fc6
srn271 <soerens2701@gmail.com>
2026-04-19 22:14:08 +0200

    feat: tsup replaced with tsdown to support TypeScript 6.0

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
+  target: false,
 });
